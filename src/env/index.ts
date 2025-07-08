4d677b2a360290db9d252ffab050da20da2c06b4
wseysouza <wseysouza@gmail.com>
2025-07-07 23:20:09 -0300

    feat: production config

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,7 +4,8 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
